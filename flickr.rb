2bc15e8f23f55b39fad19bcdbab59a315830de7f
Ian Vaughan <ianrvaughan@gmail.com>
2011-02-27 21:13:10 +0000

    more code

--- flickr.rb
+++ flickr.rb
@@ -12,25 +12,24 @@ def load_keys(file)
   keys = {} #  keys = Hash.new {0}
   File.foreach(file) do |line|
     name, value = line.split(':')
-    keys[name] = value
+    keys[name.to_sym] = value.to_s
   end
   keys
 end
 
-puts "Loading keys..."
+log.debug "Loading keys..."
 keys = load_keys('api_key.txt')
 
-puts "Fleakr init..."
-#Fleakr.api_key = "#{keys['api']}"
-Fleakr.api_key = '304ae3719ef4fb2c91557c5e553f1e9c'
+log.debug "Fleakr init..."
+Fleakr.api_key = "#{keys[:api]}"
+#Fleakr.api_key = '304ae3719ef4fb2c91557c5e553f1e9c'
 
-puts "Finding user..."
+log.debug "Finding user..."
 user = Fleakr.user('Ian Vaughan')
 
 if user
-  puts "User : #{user}"
-  puts "User->id:#{user.id}"
-  puts "User->username:#{user.username}"
+  log.debug "User->id:#{user.id}"
+  log.debug "User->username:#{user.username}"
 #  puts "Count : #{user.photos.count}"
 #  puts "title :" + user.photos.first.title
 #  puts "url :" + user.photos.first.small.url
@@ -39,32 +38,50 @@ if user
 #  user.photos.first.original.save_to('.')
 
   # loop user.sets
-  puts "Getting sets..."
+  log.debug "Getting sets..."
   sets = user.sets
-  # set =
-  sets.each do |set|
-    puts set
-    puts "Set id : #{set.id}"
-    puts "Set title : #{set.title}"
-    puts "Set count : #{set.count}"
 
-    puts "Getting photos from set..."
-    photos = set.photos
+  if sets
+    number_sets = sets.count
+    log.debug "Number of sets:#{number_sets}"
 
-    photos.each do |photo|
-      puts "Photo->id:#{photo.id}"
-      puts "Photo->title:#{photo.title}"
-      puts "Photo->secret:#{photo.secret}"
-      puts "Photo->farm_id:#{photo.farm_id}"
-      puts "Photo->server_id_id:#{photo.server_id}"
-      puts "Photo->farm_id:#{photo.farm_id}"
-      puts "Photo->original.url:#{photo.original.url}"
+    set_number = 0
+
+    sets.each do |set|
+      #puts set
+      log.debug "------------------------------------------------------------"
+      log.debug "set:#{set_number}/#{number_sets}"
+      set_number += 1
+      log.debug "Set id : #{set.id}"
+      log.debug "Set title : #{set.title}"
+      log.debug "Set count : #{set.count}"
 
 
+      log.debug ""
+      log.debug "Getting photos from set..."
+      photos = set.photos
+
+      if photos
+        number_photos = photos.count
+        log.debug "Number of photos:#{number_photos}"
+        photo_number = 0
+
+        photos.each do |photo|
+          log.debug "photo:#{photo_number}/#{number_photos}"
+          photo_number+=1
+
+          log.debug "Photo->id:#{photo.id}"
+          log.debug "Photo->title:#{photo.title}"
+          log.debug "Photo->secret:#{photo.secret}"
+          log.debug "Photo->farm_id:#{photo.farm_id}"
+          log.debug "Photo->server_id_id:#{photo.server_id}"
+          log.debug "Photo->farm_id:#{photo.farm_id}"
+          log.debug "Photo->original.url:#{photo.original.url}"
+          log.debug ""
+        end
+
+      end
     end
-
-
   end
-
 end
 
